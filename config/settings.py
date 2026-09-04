6b0c2c75b42fff21636fff86f4366c80fd048932
anwar <anwarnsyd@gmail.com>
2026-09-05 00:50:57 +0530

    feat: add JWT authentication and protected profile API

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,17 @@ MAILERS = {
 }
 
 
+# for jwt 
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
