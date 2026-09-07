f37159cfba1f808a2701bb2ea1d2e80a9ad80dc8
anwar <anwarnsyd@gmail.com>
2026-09-07 10:25:20 +0530

    refactor: improve user admin configuration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "first_name", "last_name", "is_verified")
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("is_verified",)
